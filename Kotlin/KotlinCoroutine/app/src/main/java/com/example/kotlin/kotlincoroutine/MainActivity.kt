958790a610c98298b81410c38589febae458fa02
Zayn An Nisa <shobuz_cse03@yahoo.com>
2018-10-10 00:07:06 +0600

    update from home

---
+++ Kotlin/KotlinCoroutine/app/src/main/java/com/example/kotlin/kotlincoroutine/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.kotlin.kotlincoroutine
+
+import android.app.DownloadManager
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.newFixedThreadPoolContext
+
+class MainActivity : AppCompatActivity() {
+    private val background = newFixedThreadPoolContext(2, "bg")
+    private lateinit var job: Job
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener {
+            job = launch(background) {
+                val result = requestData()
+                launch(UI) {
+                    hello.text = result
+                }
+            }
+        }
+
+    }
+    private suspend fun requestData(): String {
+        Log.d("MainActivity", "Co - routines start")
+        Thread.sleep(3000)
+        Log.d("MainActivity", "Co - routines finish")
+
+        return "DONE"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
